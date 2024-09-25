6cc266b54ab2ed4051217aeddf4e025b3e762a65
Oscar Otero <oom@oscarotero.com>
2024-09-25 12:13:18 +0200

    first version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog](https://keepachangelog.com/)
-and this project adheres to [Semantic Versioning](https://semver.org/).
+The format is based on [Keep a Changelog](https://keepachangelog.com/) and this
+project adheres to [Semantic Versioning](https://semver.org/).
 
-## 0.1.0 - Unreleased
+## [0.1.0] - 2024-09-25
 First version
+
+[0.1.0]: https://github.com/oscarotero/vscode-css-patterns/releases/tag/v0.1.0
--- src/extension.ts
+++ src/extension.ts
@@ -6,27 +6,20 @@ import fetch from "node-fetch";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-  const patternNames = await await getJson(
-    "https://css-patterns.deno.dev/",
-  ) as string[];
-
-  const patterns = await Promise.all(
-    patternNames.map((name) =>
-      getJson(`https://css-patterns.deno.dev/${name}`)
-    ),
+  const patterns = await await getJson(
+    "https://api.css.gal/",
   ) as Pattern[];
 
   const completions = patterns.map((pattern) => {
-    const snippet = new vscode.CompletionItem(`@ptr ${pattern.name}`);
-    const text = pattern.style.replaceAll(
+    const snippet = new vscode.CompletionItem(`_css:${pattern.name}`);
+    const text = pattern.css.replaceAll(
       ".host",
       ".${1:" + pattern.name + "}",
     );
     snippet.insertText = new vscode.SnippetString(text);
-    const docs: any = new vscode.MarkdownString(`
-    ${pattern.description}
-    Preview: https://css-patterns.deno.dev/${pattern.name}
-  `);
+    const docs: any = new vscode.MarkdownString(`${pattern.description}
+Preview: https://api.css.gal/${pattern.name}
+`);
     snippet.documentation = docs;
     return snippet;
   });
@@ -48,9 +41,8 @@ export function deactivate() {}
 
 interface Pattern {
   name: string;
-  description?: string;
-  style: string;
-  content: string;
+  description: string;
+  css: string;
 }
 
 async function getJson(url: string) {
